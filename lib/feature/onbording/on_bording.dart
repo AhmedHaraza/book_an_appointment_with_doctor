c0eb76297d464e3cbd784cfd19dd74ecaf43c1ae
Ahmed Haraza <ahmed.haraza755@gmail.com>
2024-05-10 19:05:19 +0300

    gust a fack push to

---
+++ lib/feature/onbording/on_bording.dart
@@ -0,0 +1,43 @@
+import 'package:book_an_appointment_with_doctor/Core/theming/styles.dart';
+import 'package:book_an_appointment_with_doctor/feature/onbording/widgets/docLogo_and_name.dart';
+import 'package:book_an_appointment_with_doctor/feature/onbording/widgets/doctor_text_and_image.dart';
+import 'package:book_an_appointment_with_doctor/feature/onbording/widgets/get_started_boutton.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class OnBordingScreen extends StatelessWidget {
+  const OnBordingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(top: 30.h),
+        child: Column(
+            
+          children: [
+            const DocLogoAndName(),
+            SizedBox(height: 30.h),
+            const DoctorTextAndImage(),
+            SizedBox(height: 10.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.w),
+              child: Column(
+                children: [
+                  Text(
+                    textAlign: TextAlign.center,
+                    "Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.",
+                    style: TextStyles.font13grayRegular,
+                  ),
+                  SizedBox(height: 30.h),
+                  const GetStartedBoutton(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
